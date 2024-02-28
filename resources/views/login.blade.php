690fd5cadc8979d4cded8f674d2a87ecd1b6d73e
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2024-02-28 14:49:23 +0700

    Update
    Update composer
    Update Web
    Add Login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,6 @@
+<center><h1>Hello World</h1></center>
+<center><h1>Hello World</h1></center>
+<center><h1>Hello World</h1></center>
+<center><h1>Hello World</h1></center>
+<center><h1>Hello World</h1></center>
+<center><button>Press Me :D </button></center>
