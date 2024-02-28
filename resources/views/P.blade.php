6d653afb3f3db0a4733e0de59dc426ad47d19d65
Bob0I0 <159877192+Bob0I0@users.noreply.github.com>
2024-02-28 15:01:52 +0700

    Adding P.blade.php

---
+++ resources/views/P.blade.php
@@ -0,0 +1,4 @@
+<center><h1>Welcome</h1></center>
+<h1> </h1>
+<center><h3>Disini kosong :) </h3></center>
+<center><button>Press Me :D</button></center>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,4 +3,4 @@
 <center><h1>Hello World</h1></center>
 <center><h1>Hello World</h1></center>
 <center><h1>Hello World</h1></center>
-<center><button>Press Me :D </button></center>
+<center><button>Press Me :D</button></center>
